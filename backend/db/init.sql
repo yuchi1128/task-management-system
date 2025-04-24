7549960a994291e370811764a1a15d16e50bb6fd
yuchi1128 <yukiya20021128@icloud.com>
2025-04-24 05:47:02 +0000

    DBテーブルの作成

---
+++ backend/db/init.sql
@@ -0,0 +1,10 @@
+echo "CREATE TABLE tasks (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ description TEXT,
+ start_date TIMESTAMP,
+ end_date TIMESTAMP,
+ priority VARCHAR(10) CHECK (priority IN ('High', 'Middle', 'Low')),
+ status VARCHAR(20) CHECK (status IN ('NotStarted', 'InProgress', 'Completed')),
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
